7c69673bbd29a71099607e796f6314bf5bbcf711
Stanislav Kozhemyakin <st.kozhemyakin@gmail.com>
2026-01-22 22:01:38 +0200

    Added a form, list of employees, form for adding an employee to the list

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,6 +1,9 @@
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
+import EmployeesList from '../employees-list/employees-list'
+import EmployeesAddForm from '../employees-add-form/employees-add-form';
+
 import './app.css';
 
 function App() {
@@ -13,6 +16,8 @@ function App() {
             <AppFilter/>
         </div>
 
+        <EmployeesList/>
+        <EmployeesAddForm/>
      </div>   
     )
 }
